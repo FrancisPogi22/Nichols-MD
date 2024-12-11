e7cd540b8e862f05541de6fa12ac9d15364ea453
Francis Cabusas <cabusasfg779@gmail.com>
2024-12-11 14:07:51 +0800

    Update System

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,191 @@
-$(document).ready(()=>{function t(){$(window).scrollTop()>50?$("#header").addClass("scrolled"):$("#header").removeClass("scrolled")}function a(){$(window).width()>1310&&$("#mobile-navbar").css("display","none"),1310>$(window).width()&&$("#skincare").css("display","none")}async function e(t){await fetch("/cart/add",{method:"post",body:new FormData(t)}),await n().then(()=>{i()})}async function n(){$.ajax({type:"GET",url:"/cart",dataType:"html",success(t){let a=$(t).find(".cart-drawer").html();$(".cart-drawer").html(a),s()}})}function i(){$(".cart-drawer-con").addClass("active"),$(".cart-drawer").addClass("active")}function c(){$(".cart-drawer-con").removeClass("active"),$(".cart-drawer").removeClass("active")}function s(){$(".closeCart").click(function(){c()}),$(".remove-item-btn").click(function(t){t.preventDefault(),t.stopPropagation();let a=$(this).data("variant");$.ajax({type:"POST",url:"/cart/change.js",dataType:"json",data:{quantity:0,id:a},async success(){n();let t=await fetch("/cart.js"),a=await t.json();o(a.item_count)}})})}async function o(){let t=await fetch("/cart.js"),a=await t.json();$(".cart-item-count span").each(function(){$(this).text(a.item_count)})}function r(t){let a=$(".cart-subtotal"),e=$(".cart-total-price");a.length&&a.text(l(t.items_subtotal_price)),e.length&&e.text(l(t.total_price))}function l(t,a="$"){let e=(t/100).toFixed(2);return`${a}${e.replace(/\B(?=(\d{3})+(?!\d))/g,",")}`}window.addEventListener("load",()=>{$(".loader").addClass("loader--hidden")}),$(window).scroll(()=>{t()}),t(),$(".search").click(t=>{t.stopPropagation(),$(".search-con").toggle()}),$("#header .has-sub-menu").each(function(){let t=!1,a=null,e=$(this),n=$(this).data("title"),i=$(`#${n}`);e.hover(function(){t&&a===n||(a&&a!==n&&($(`#${a}`).stop(!0,!0).slideUp(400),$(`[data-title="${a}"]`).removeClass("active")),e.addClass("active"),i.stop(!0,!0).slideDown(400),a=n)},function(){i.is(":hover")||(i.stop(!0,!0).slideUp(400),e.removeClass("active"),a=null)}),e.on("click",function(c){t?(window.location.href=e.find("a").attr("href"),t=!1,a=null):(c.preventDefault(),e.addClass("active"),i.stop(!0,!0).slideDown(400),t=!0,a=n)});let c;i.hover(function(){clearTimeout(c),e.addClass("active"),i.stop(!0,!0).slideDown(400)},function(){c=setTimeout(()=>{e.is(":hover")||i.is(":hover")||(i.stop(!0,!0).slideUp(400),e.removeClass("active"),a=null,t=!1)},200)})}),$("#mobile-navbar .has-sub-menu").on("click",function(t){t.preventDefault(),t.stopPropagation();let a=$(this).find(".sub-link-container"),e=a.is(":visible");$(this).toggleClass("active"),$(".sub-link-container").slideUp(),e?window.location.href=$(this).children("a").attr("href"):a.slideDown()}),$(".add-to-cart-form").each(function(){$(this).on("submit",async function(t){t.preventDefault();try{await e(this).then(()=>{o()})}catch(a){console.error("Error adding item to cart:",a)}})}),$(window).on("resize",function(){a()}),$(".mobile-btn-container").click(function(){$("#mobile-navbar").slideToggle()}),s(),$('#header a[href="/cart"]').on("click",t=>{t.preventDefault(),i()}),$(".cart-drawer").on("click",".increase-quantity, .decrease-quantity",function(t){t.preventDefault();let a=$(this),e=a.closest(".quantity-text").find(".quantity-value"),n=a.data("line"),i=parseInt(e.text(),10),c=a.hasClass("increase-quantity")?i+1:Math.max(1,i-1);$.ajax({url:"/cart/change.js",type:"POST",dataType:"json",data:JSON.stringify({line:parseInt(n,10),quantity:c}),contentType:"application/json",success(t){e.text(c),r(t),o()},error(){console.error("Error updating cart:",error)}})})});
+$(document).ready(() => {
+  function t() {
+    $(window).scrollTop() > 50
+      ? $("#header").addClass("scrolled")
+      : $("#header").removeClass("scrolled");
+  }
+  function a() {
+    $(window).width() > 1310 && $("#mobile-navbar").css("display", "none"),
+      1310 > $(window).width() && $("#skincare").css("display", "none");
+  }
+  async function e(t) {
+    await fetch("/cart/add", { method: "post", body: new FormData(t) }),
+      await n().then(() => {
+        i();
+      });
+  }
+  async function n() {
+    $.ajax({
+      type: "GET",
+      url: "/cart",
+      dataType: "html",
+      success(t) {
+        let a = $(t).find(".cart-drawer").html();
+        $(".cart-drawer").html(a), s();
+      },
+    });
+  }
+  function i() {
+    $(".cart-drawer-con").addClass("active"),
+      $(".cart-drawer").addClass("active");
+  }
+  function c() {
+    $(".cart-drawer-con").removeClass("active"),
+      $(".cart-drawer").removeClass("active");
+  }
+  function s() {
+    $(".closeCart").click(function () {
+      c();
+    }),
+      $(".remove-item-btn").click(function (t) {
+        t.preventDefault(), t.stopPropagation();
+        let a = $(this).data("variant");
+        $.ajax({
+          type: "POST",
+          url: "/cart/change.js",
+          dataType: "json",
+          data: { quantity: 0, id: a },
+          async success() {
+            n();
+            let t = await fetch("/cart.js"),
+              a = await t.json();
+            o(a.item_count);
+          },
+        });
+      });
+  }
+  async function o() {
+    let t = await fetch("/cart.js"),
+      a = await t.json();
+    $(".cart-item-count span").each(function () {
+      $(this).text(a.item_count);
+    });
+  }
+  function r(t) {
+    let a = $(".cart-subtotal"),
+      e = $(".cart-total-price");
+    a.length && a.text(l(t.items_subtotal_price)),
+      e.length && e.text(l(t.total_price));
+  }
+  function l(t, a = "$") {
+    let e = (t / 100).toFixed(2);
+    return `${a}${e.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+  }
+  window.addEventListener("load", () => {
+    $(".loader").addClass("loader--hidden");
+  }),
+    $(window).scroll(() => {
+      t();
+    }),
+    t(),
+    $(".search").click((t) => {
+      t.stopPropagation(), $(".search-con").toggle();
+    }),
+    $("#header .has-sub-menu").each(function () {
+      let t = !1,
+        a = null,
+        e = $(this),
+        n = $(this).data("title"),
+        i = $(`#${n}`);
+      e.hover(
+        function () {
+          (t && a === n) ||
+            (a &&
+              a !== n &&
+              ($(`#${a}`).stop(!0, !0).slideUp(400),
+              $(`[data-title="${a}"]`).removeClass("active")),
+            e.addClass("active"),
+            i.stop(!0, !0).slideDown(400),
+            (a = n));
+        },
+        function () {
+          i.is(":hover") ||
+            (i.stop(!0, !0).slideUp(400), e.removeClass("active"), (a = null));
+        }
+      ),
+        e.on("click", function (c) {
+          t
+            ? ((window.location.href = e.find("a").attr("href")),
+              (t = !1),
+              (a = null))
+            : (c.preventDefault(),
+              e.addClass("active"),
+              i.stop(!0, !0).slideDown(400),
+              (t = !0),
+              (a = n));
+        });
+      let c;
+      i.hover(
+        function () {
+          clearTimeout(c), e.addClass("active"), i.stop(!0, !0).slideDown(400);
+        },
+        function () {
+          c = setTimeout(() => {
+            e.is(":hover") ||
+              i.is(":hover") ||
+              (i.stop(!0, !0).slideUp(400),
+              e.removeClass("active"),
+              (a = null),
+              (t = !1));
+          }, 200);
+        }
+      );
+    }),
+    $("#mobile-navbar .has-sub-menu").on("click", function (t) {
+      t.preventDefault(), t.stopPropagation();
+      let a = $(this).find(".sub-link-container"),
+        e = a.is(":visible");
+      $(this).toggleClass("active"),
+        $(".sub-link-container").slideUp(),
+        e
+          ? (window.location.href = $(this).children("a").attr("href"))
+          : a.slideDown();
+    }),
+    $(".add-to-cart-form").each(function () {
+      $(this).on("submit", async function (t) {
+        t.preventDefault();
+        try {
+          await e(this).then(() => {
+            o();
+          });
+        } catch (a) {
+          console.error("Error adding item to cart:", a);
+        }
+      });
+    }),
+    $(window).on("resize", function () {
+      a();
+    }),
+    $(".mobile-btn-container").click(function () {
+      $("#mobile-navbar").slideToggle();
+    }),
+    s(),
+    $('#header a[href="/cart"]').on("click", (t) => {
+      t.preventDefault(), i();
+    }),
+    $(".cart-drawer").on(
+      "click",
+      ".increase-quantity, .decrease-quantity",
+      function (t) {
+        t.preventDefault();
+        let a = $(this),
+          e = a.closest(".quantity-text").find(".quantity-value"),
+          n = a.data("line"),
+          i = parseInt(e.text(), 10),
+          c = a.hasClass("increase-quantity") ? i + 1 : Math.max(1, i - 1);
+        $.ajax({
+          url: "/cart/change.js",
+          type: "POST",
+          dataType: "json",
+          data: JSON.stringify({ line: parseInt(n, 10), quantity: c }),
+          contentType: "application/json",
+          success(t) {
+            e.text(c), r(t), o();
+          },
+          error() {
+            console.error("Error updating cart:", error);
+          },
+        });
+      }
+    );
+});
--- assets/filterProduct.js
+++ assets/filterProduct.js
@@ -1,1 +1,178 @@
-class ProductWidget{constructor(t){this.collection=t||"treatments",this.productList=$(".categories-product-list"),this.productsPerPage=9,this.currentPage=1,this.allProducts=[],this.totalPages=0,this.init()}init(){this.bindEvents(),this.fetchFilteredProducts()}bindEvents(){$("#priceRange").on("mouseup",()=>{let t=$("#priceRange").val();$("#minPrice").text(t),this.fetchFilteredProducts({minPrice:0,maxPrice:parseInt(t,10)})}),$("#filterBtn").click(t=>{t.preventDefault();let e=$("#skinTypeFilter").val(),r=$("#productTypeFilter").val(),a=$("#brandFilter").val();this.fetchFilteredProducts({skinType:e,productType:r,brand:a})}),$("#sortBy").change(t=>{let e=$(t.currentTarget).val();this.fetchFilteredProducts({sortBy:e})})}createProductWidget(t){let e=`/products/${t.handle}`,r=$("<div>",{class:"product-widget"}),a=$("<a>",{href:e}),i;i=t.images&&t.images[0]?.src?$("<img>",{src:t.images[0].src,alt:t.images[0].alt||"Product Image"}):$("<img>",{src:"https://cdn.shopify.com/s/files/1/0597/1116/0382/files/NMD_Logo.png?v=1732133643",alt:"Default Image"}),a.append(i),r.append(a);let s=$("<div>",{class:"product-details"}),c=$("<a>",{href:e}).append($("<p>").text(t.title||"Treatment Name"));parseFloat(t.variants[0].price);let o=t.variants[0]?.compare_at_price?parseFloat(t.variants[0].compare_at_price):null,d=Math.min(...t.variants.map(t=>t.price)),n=Math.max(...t.variants.map(t=>t.price)),l=$("<p>",{class:d!==n?"price-comparison":""}).append(d!==n?$("<span>").text("$"+d.toFixed(2)+" - $"+n.toFixed(2)):o&&o<d?[$("<span>").text("$"+o.toFixed(2)),$("<span>",{class:"compare-price"}).text(" $"+d.toFixed(2)).css("text-decoration","line-through"),]:$("<span>").text("$"+d.toFixed(2))),p=$("<a>",{href:e,class:"btn-secondary"}).text("View Product");return s.append(c,l,p),r.append(s),r}fetchFilteredProducts(t={}){this.showLoader();let e=`/collections/${this.collection}/products.json`;$.ajax({type:"GET",url:e,success:e=>{let r=e.products;if(this.allProducts=r.filter(e=>{let r=parseFloat(e.variants[0].price),a=(void 0===t.minPrice||r>=t.minPrice)&&(void 0===t.maxPrice||r<=t.maxPrice),i=!t.skinType||e.tags?.includes(t.skinType),s=!t.productType||e.tags?.includes(t.productType),c=!t.brand||e.tags?.includes(t.brand);return a&&i&&s&&c}),t.sortBy)switch(t.sortBy){case"title-asc":this.allProducts.sort((t,e)=>t.title.localeCompare(e.title));break;case"title-desc":this.allProducts.sort((t,e)=>e.title.localeCompare(t.title));break;case"price-asc":this.allProducts.sort((t,e)=>parseFloat(t.variants[0].price)-parseFloat(e.variants[0].price));break;case"price-desc":this.allProducts.sort((t,e)=>parseFloat(e.variants[0].price)-parseFloat(t.variants[0].price))}this.totalPages=Math.ceil(this.allProducts.length/this.productsPerPage),this.currentPage=1,this.renderProducts(),this.renderPagination(),this.hideLoader()},error:(t,e,r)=>{console.error("Error fetching collection data:",r),this.hideLoader()}})}renderPagination(){let t=$(".pagination-container");if(t.empty(),this.totalPages>1){let e=$("<ul>",{class:"pagination"});for(let r=1;r<=this.totalPages;r++){let a=$("<span>",{class:r===this.currentPage?"pagination-current":""}),i=$("<a>",{href:"#!",text:r,class:r!==this.currentPage?"pagination-link":"","data-page":r});a.append(i),e.append(a)}t.append(e),$(".pagination-link").click(t=>{t.preventDefault();let e=$(t.currentTarget).data("page");this.currentPage=e,this.renderProducts(),this.renderPagination()})}}renderProducts(){this.productList.empty();let t=(this.currentPage-1)*this.productsPerPage,e=t+this.productsPerPage,r=this.allProducts.slice(t,e);r.length>0?r.forEach(t=>{let e=this.createProductWidget(t);this.productList.append(e)}):this.productList.append($("<p>",{class:"no-products-message",text:"No products found with the selected filters."}))}showLoader(){$(".product-loader").removeClass("loader--hidden")}hideLoader(){$(".product-loader").addClass("loader--hidden")}}
+class ProductWidget {
+  constructor(t) {
+    (this.collection = t || "treatments"),
+      (this.productList = $(".categories-product-list")),
+      (this.productsPerPage = 9),
+      (this.currentPage = 1),
+      (this.allProducts = []),
+      (this.totalPages = 0),
+      this.init();
+  }
+  init() {
+    this.bindEvents(), this.fetchFilteredProducts();
+  }
+  bindEvents() {
+    $("#priceRange").on("mouseup", () => {
+      let t = $("#priceRange").val();
+      $("#minPrice").text(t),
+        this.fetchFilteredProducts({ minPrice: 0, maxPrice: parseInt(t, 10) });
+    }),
+      $("#filterBtn").click((t) => {
+        t.preventDefault();
+        let e = $("#skinTypeFilter").val(),
+          r = $("#productTypeFilter").val(),
+          a = $("#brandFilter").val();
+        this.fetchFilteredProducts({ skinType: e, productType: r, brand: a });
+      }),
+      $("#sortBy").change((t) => {
+        let e = $(t.currentTarget).val();
+        this.fetchFilteredProducts({ sortBy: e });
+      });
+  }
+  createProductWidget(t) {
+    let e = `/products/${t.handle}`,
+      r = $("<div>", { class: "product-widget" }),
+      a = $("<a>", { href: e }),
+      i;
+    (i =
+      t.images && t.images[0]?.src
+        ? $("<img>", {
+            src: t.images[0].src,
+            alt: t.images[0].alt || "Product Image",
+          })
+        : $("<img>", {
+            src: "https://cdn.shopify.com/s/files/1/0597/1116/0382/files/NMD_Logo.png?v=1732133643",
+            alt: "Default Image",
+          })),
+      a.append(i),
+      r.append(a);
+    let s = $("<div>", { class: "product-details" }),
+      c = $("<a>", { href: e }).append(
+        $("<p>").text(t.title || "Treatment Name")
+      );
+    parseFloat(t.variants[0].price);
+    let o = t.variants[0]?.compare_at_price
+        ? parseFloat(t.variants[0].compare_at_price)
+        : null,
+      d = Math.min(...t.variants.map((t) => t.price)),
+      n = Math.max(...t.variants.map((t) => t.price)),
+      l = $("<p>", { class: d !== n ? "price-comparison" : "" }).append(
+        d !== n
+          ? $("<span>").text("$" + d.toFixed(2) + " - $" + n.toFixed(2))
+          : o && o < d
+          ? [
+              $("<span>").text("$" + o.toFixed(2)),
+              $("<span>", { class: "compare-price" })
+                .text(" $" + d.toFixed(2))
+                .css("text-decoration", "line-through"),
+            ]
+          : $("<span>").text("$" + d.toFixed(2))
+      ),
+      p = $("<a>", { href: e, class: "btn-secondary" }).text("View Product");
+    return s.append(c, l, p), r.append(s), r;
+  }
+  fetchFilteredProducts(t = {}) {
+    this.showLoader();
+    let e = `/collections/${this.collection}/products.json`;
+    $.ajax({
+      type: "GET",
+      url: e,
+      success: (e) => {
+        let r = e.products;
+        if (
+          ((this.allProducts = r.filter((e) => {
+            let r = parseFloat(e.variants[0].price),
+              a =
+                (void 0 === t.minPrice || r >= t.minPrice) &&
+                (void 0 === t.maxPrice || r <= t.maxPrice),
+              i = !t.skinType || e.tags?.includes(t.skinType),
+              s = !t.productType || e.tags?.includes(t.productType),
+              c = !t.brand || e.tags?.includes(t.brand);
+            return a && i && s && c;
+          })),
+          t.sortBy)
+        )
+          switch (t.sortBy) {
+            case "title-asc":
+              this.allProducts.sort((t, e) => t.title.localeCompare(e.title));
+              break;
+            case "title-desc":
+              this.allProducts.sort((t, e) => e.title.localeCompare(t.title));
+              break;
+            case "price-asc":
+              this.allProducts.sort(
+                (t, e) =>
+                  parseFloat(t.variants[0].price) -
+                  parseFloat(e.variants[0].price)
+              );
+              break;
+            case "price-desc":
+              this.allProducts.sort(
+                (t, e) =>
+                  parseFloat(e.variants[0].price) -
+                  parseFloat(t.variants[0].price)
+              );
+          }
+        (this.totalPages = Math.ceil(
+          this.allProducts.length / this.productsPerPage
+        )),
+          (this.currentPage = 1),
+          this.renderProducts(),
+          this.renderPagination(),
+          this.hideLoader();
+      },
+      error: (t, e, r) => {
+        console.error("Error fetching collection data:", r), this.hideLoader();
+      },
+    });
+  }
+  renderPagination() {
+    let t = $(".pagination-container");
+    if ((t.empty(), this.totalPages > 1)) {
+      let e = $("<ul>", { class: "pagination" });
+      for (let r = 1; r <= this.totalPages; r++) {
+        let a = $("<span>", {
+            class: r === this.currentPage ? "pagination-current" : "",
+          }),
+          i = $("<a>", {
+            href: "#!",
+            text: r,
+            class: r !== this.currentPage ? "pagination-link" : "",
+            "data-page": r,
+          });
+        a.append(i), e.append(a);
+      }
+      t.append(e),
+        $(".pagination-link").click((t) => {
+          t.preventDefault();
+          let e = $(t.currentTarget).data("page");
+          (this.currentPage = e),
+            this.renderProducts(),
+            this.renderPagination();
+        });
+    }
+  }
+  renderProducts() {
+    this.productList.empty();
+    let t = (this.currentPage - 1) * this.productsPerPage,
+      e = t + this.productsPerPage,
+      r = this.allProducts.slice(t, e);
+    r.length > 0
+      ? r.forEach((t) => {
+          let e = this.createProductWidget(t);
+          this.productList.append(e);
+        })
+      : this.productList.append(
+          $("<p>", {
+            class: "no-products-message",
+            text: "No products found with the selected filters.",
+          })
+        );
+  }
+  showLoader() {
+    $(".product-loader").removeClass("loader--hidden");
+  }
+  hideLoader() {
+    $(".product-loader").addClass("loader--hidden");
+  }
+}
